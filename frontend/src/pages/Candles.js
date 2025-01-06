815521a98fa2970258d6454eeb5dac0b8b0eb618
Stella Ndjikessi <stelladiana@live.fr>
2025-01-06 14:20:13 +0100

    More candles added

--- frontend/src/pages/Candles.js
+++ frontend/src/pages/Candles.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchProducts } from "../enpoints";
+import Navbar from '../components/Navbar';
 
 const Candles = () => {
   const [products, setProducts] = useState([]);
@@ -14,8 +15,9 @@ const Candles = () => {
 
   return(
     <>
+      <Navbar/>
       <h1 className="text-4xl font-bold text-center">OUR CANDLES</h1>
-      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 m-8">
         {products.map((product) =>
           <div key={product.id}>
             <img src={product.image} alt='{product.name}'/>
